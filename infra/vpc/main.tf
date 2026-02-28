5e5d817a50039a13b78ffcc5d9d3eaf0ff469b32
sharath <sharathraghav2003@gmail>
2026-02-28 17:58:57 +0530

    infra changes done

---
+++ infra/vpc/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  vpc_cidr           = var.vpc_cidr
+  public_subnets     = var.public_subnets
+  private_subnets    = var.private_subnets
+  availability_zones = var.availability_zones
+  environment        = var.environment
+}
